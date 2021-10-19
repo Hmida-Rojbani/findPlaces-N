53682e1d8b0ee9cbbfb370953cf08e4b4da848e7
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-19 18:54:36 +0100

    first commit

---
+++ src/app/places/offers/offers-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OffersPage } from './offers.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersPage
+  },
+  {
+    path: 'new-offer',
+    loadChildren: () => import('./new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class OffersPageRoutingModule {}
